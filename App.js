efda2dfd4173ebb80e4866acd7a233e523e71b5c
Ayanyiola Enoch <ayanyiolaenoch155@gmail.com>
2025-11-06 16:04:13 +0100

    fixed and adjusted viewing app issues

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React, { useEffect } from "react";
 import { ActivityIndicator, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import Toast from "react-native-toast-message";
 import toastConfig from "./src/constants/toastConfig";
-import MyStack from "./src/navigation/MyStack";
+import MyStack from "./src/navigator/MyStack";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -28,31 +30,27 @@ export default function App() {
 
   if (!fontsLoaded) {
     return (
-      <View
-        style={{
-          flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <ActivityIndicator />
       </View>
     );
   }
 
   return (
-    <>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="Root" component={MyStack} />
-        </Stack.Navigator>
-      </NavigationContainer>
-      <Toast
-        config={toastConfig}
-        position="top"
-        topOffset={32}
-        visibilityTime={2200}
-      />
-    </>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Root" component={MyStack} />
+          </Stack.Navigator>
+        </NavigationContainer>
+        <Toast
+          config={toastConfig}
+          position="top"
+          topOffset={32}
+          visibilityTime={2200}
+        />
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
