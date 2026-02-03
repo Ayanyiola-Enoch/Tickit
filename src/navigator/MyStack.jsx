d8aeb9d7c63c71a7bf36a366842bb9bdc320e08d
Ayanyiola Enoch <ayanyiolaenoch155@gmail.com>
2026-02-03 13:19:20 +0100

    adjusting the tickit packages

--- src/navigator/MyStack.jsx
+++ src/navigator/MyStack.jsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "../screens/Home";
-// import About from "../screens/About";
 
 const Stack = createNativeStackNavigator();
 
